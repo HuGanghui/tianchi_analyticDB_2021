a65f2ab5df2eec4c37057c6380f8a92c0fa13475
HuGanghui <huganghui18@gmail.com>
2021-06-09 22:09:31 +0800

    增加AnalyticDB接口的实现类: MergeSortAnalyticDB以及单元测试

---
+++ src/main/java/com/aliyun/adb/contest/MergeSortAnalyticDB.java
@@ -0,0 +1,210 @@
+package com.aliyun.adb.contest;
+
+import com.aliyun.adb.contest.spi.AnalyticDB;
+
+import java.io.*;
+import java.util.*;
+
+import static com.aliyun.adb.contest.common.Utils.bytesToLong;
+import static com.aliyun.adb.contest.common.Utils.longToBytes;
+
+public class MergeSortAnalyticDB implements AnalyticDB {
+
+    private Map<String, List<File>> fileMap = new HashMap<>();
+    private Map<String, File> sortedFileMap = new HashMap<>();
+    // 每个文件可保存的最大行数
+    private final int MAX_FILE_CAP = (int) (0.375 * Math.pow(10, 8));
+    private final int TOTAL_LINE = (int) (3 * Math.pow(10, 8));
+//    private final int MAX_FILE_CAP = (int) (2500);
+
+    private final int Buffer_CAP = 4096;
+    /**
+     *
+     * The implementation must contain a public no-argument constructor.
+     *
+     */
+    public MergeSortAnalyticDB() {
+    }
+
+    @Override
+    public void load(String tpchDataFileDir, String workspaceDir) throws Exception {
+        File dir = new File(tpchDataFileDir);
+
+        for (File dataFile : dir.listFiles()) {
+            System.out.println("Start loading table " + dataFile.getName());
+
+//            // You can write data to workspaceDir
+            saveToDisk(workspaceDir, dataFile);
+
+//            loadInMemroy(dataFile);
+        }
+
+        for (String key : fileMap.keySet()) {
+            sort(key, workspaceDir);
+        }
+        loadInMemroy();
+    }
+
+    @Override
+    public String quantile(String table, String column, double percentile) throws Exception {
+        String tableColumn = tableColumnKey(table, column);
+        RandomAccessFile raf = new RandomAccessFile(sortedFileMap.get(tableColumn), "r");
+        long offset = (long) (TOTAL_LINE * percentile - 1) * 8;
+        raf.seek(offset);
+        byte[] bbuf = new byte[8];
+        raf.read(bbuf);
+        return String.valueOf(bytesToLong(bbuf));
+    }
+
+    private void saveToDisk(String workspaceDir, File dataFile) throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader(dataFile));
+        String table = dataFile.getName();
+        String[] columns = reader.readLine().split(",");
+        final int columnLength = columns.length;
+        int file_index = 0;
+        for (String column : columns) {
+            String tableColumn = tableColumnKey(table, column);
+            List<File> tempList = new ArrayList<>();
+            fileMap.put(tableColumn, tempList);
+        }
+
+        long[][] valuesToSort = new long[columnLength][MAX_FILE_CAP];
+        int valuesToSortIndex = 0;
+        String rawRow;
+        FileOutputStream[] outArray = new FileOutputStream[columnLength];
+        for (int i = 0; i < columnLength; i++) {
+            String tableColumn = tableColumnKey(table, columns[i]);
+            File file = new File(workspaceDir, tableColumn + "_" + file_index);
+            fileMap.get(tableColumn).add(file);
+            outArray[i] = new FileOutputStream(file);
+        }
+
+        while ((rawRow = reader.readLine()) != null) {
+            String[] row = rawRow.split(",");
+            for (int i = 0; i < columnLength; i++) {
+                Long l = new Long(row[i]);
+                valuesToSort[i][valuesToSortIndex] = l;
+            }
+            valuesToSortIndex++;
+
+            if (valuesToSortIndex % MAX_FILE_CAP == 0) {
+                valuesToSortIndex = 0;
+                file_index++;
+                for (int i = 0; i < columnLength; i++) {
+                    saveToDisk(valuesToSort[i], outArray[i], true);
+                    String tableColumn = tableColumnKey(table, columns[i]);
+                    File file = new File(workspaceDir, tableColumn + "_" + file_index);
+                    fileMap.get(tableColumn).add(file);
+                    outArray[i] = new FileOutputStream(file);
+                }
+            }
+        }
+
+        for (int i = 0; i < columnLength; i++) {
+            if (valuesToSortIndex == 0) {
+                continue;
+            }
+            long[] leftToSort = new long[valuesToSortIndex];
+            System.arraycopy(valuesToSort[i], 0, leftToSort, 0, valuesToSortIndex);
+            saveToDisk(leftToSort, outArray[i], true);
+        }
+        reader.close();
+    }
+
+    private void sort(String key, String workspaceDir) throws Exception {
+        List<File> fileList = fileMap.get(key);
+        int n = fileList.size();
+        FileInputStream[] fileIns = new FileInputStream[n];
+        PriorityQueue<Pair> pq = new PriorityQueue<Pair>((o1, o2) -> {
+            if (o1.value <= o2.value) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+        for (int i = 0; i < n; i++) {
+            fileIns[i] = new FileInputStream(fileList.get(i));
+            Long temp = getSingleNumber(fileIns[i]);
+            if (temp != null) {
+                pq.add(new Pair(i, temp));
+            }
+        }
+        long[] buf = new long[MAX_FILE_CAP];
+        List<Long> forTest = new ArrayList<>();
+        File file = new File(workspaceDir, key + "_" + "sorted");
+        sortedFileMap.put(key, file);
+        FileOutputStream out = new FileOutputStream(file);
+        int buf_index = 0;
+        while (!pq.isEmpty()) {
+            Pair pair = pq.poll();
+            buf[buf_index++] = pair.value;
+            forTest.add(pair.value);
+            if (buf_index == MAX_FILE_CAP) {
+                saveToDisk(buf, out, false);
+                buf_index = 0;
+            }
+            Long temp = getSingleNumber(fileIns[pair.insIndex]);
+            if (temp != null) {
+                pq.offer(new Pair(pair.insIndex, temp));
+            }
+        }
+        if (buf_index != 0) {
+            long[] leftToSort = new long[buf_index];
+            System.arraycopy(buf, 0, leftToSort, 0, buf_index);
+            saveToDisk(leftToSort, out, true);
+        }
+    }
+
+    private void saveToDisk(long[] valuesToSort, FileOutputStream out, boolean nowClose) throws Exception {
+        Arrays.sort(valuesToSort);
+        byte[] bbuf = new byte[Buffer_CAP];
+        int bbuf_position = 0;
+        for (long l : valuesToSort) {
+            byte[] temp = longToBytes(l);
+            System.arraycopy(temp, 0, bbuf, bbuf_position, temp.length);
+            bbuf_position += temp.length;
+            if (bbuf_position == Buffer_CAP) {
+                out.write(bbuf);
+                bbuf_position = 0;
+            }
+        }
+        if (bbuf_position != 0) {
+            byte[] temp = new byte[bbuf_position];
+            System.arraycopy(bbuf, 0, temp, 0, bbuf_position);
+            out.write(temp);
+        }
+        if (nowClose) {
+            out.flush();
+            out.close();
+        }
+    }
+
+    private Long getSingleNumber(FileInputStream ins) throws Exception {
+        byte[] bbuf = new byte[8];
+        int hasRead = 0;
+        if ((hasRead = ins.read(bbuf)) > 0) {
+            return bytesToLong(bbuf);
+        } else {
+            return null;
+        }
+    }
+
+    private class Pair {
+        public int insIndex;
+        public Long value;
+        public Pair(int insIndex, Long value) {
+            this.insIndex = insIndex;
+            this.value = value;
+        }
+    }
+
+    private void loadInMemroy() {
+        System.out.println("freeMemory: " + Runtime.getRuntime().freeMemory()/1024/1024 + " M");
+        System.out.println("totalMemory: " + Runtime.getRuntime().totalMemory()/1024/1024 + " M");
+        System.out.println("maxMemory: " + Runtime.getRuntime().maxMemory()/1024/1024 + " M");
+    }
+
+    private String tableColumnKey(String table, String column) {
+        return (table + "_" + column).toLowerCase();
+    }
+}
--- src/test/java/com/aliyun/adb/contest/TestSimpleAnalyticDB.java
+++ src/test/java/com/aliyun/adb/contest/TestSimpleAnalyticDB.java
@@ -3,9 +3,8 @@ package com.aliyun.adb.contest;
 import com.aliyun.adb.contest.spi.AnalyticDB;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +14,56 @@ public class TestSimpleAnalyticDB {
     private final String SPLITE = "-";
     private String test_data_dir = "src/test/resources/com.aliyun.adb.contest/test_data/";
     private String test_result_path = "src/test/resources/com.aliyun.adb.contest/result/results";
+
+    private String test_split_file_dir = "./tmp";
+
+    @Test
+    public void testsaveToDisk() throws Exception {
+        MergeSortAnalyticDB db = new MergeSortAnalyticDB();
+        db.load(test_data_dir, "./tmp");
+    }
+
+    @Test
+    public void testSplitFileRight() throws Exception {
+        File dir = new File(test_split_file_dir);
+//        String test = "lineitem_l_orderkey";
+        String test = "lineitem_l_partkey";
+        int total = 0;
+        for (File dataFile : dir.listFiles()) {
+            if (dataFile.getName().startsWith(test)) {
+                FileInputStream fileInputStream = new FileInputStream(dataFile);
+                total += testNum(fileInputStream);
+            }
+        }
+        assertEquals(10000, total);
+    }
+
+    private int testNum(FileInputStream ins) throws Exception {
+        byte[] bbuf2 = new byte[8];
+        int hasRead = 0;
+        int num = 0;
+        while ((hasRead = ins.read(bbuf2)) > 0) {
+            num++;
+        }
+        return num;
+    }
+
+    private byte[] longToBytes(long x) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(x);
+        return buffer.array();
+    }
+
+    private long bytesToLong(byte[] bytes) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.put(bytes);
+        buffer.flip();//need flip
+        return buffer.getLong();
+    }
+
+    private String tableColumnKey(String table, String column) {
+        return (table + "." + column).toLowerCase();
+    }
 
     @Test
     public void testSimpleAnalyticDB() {
@@ -42,12 +91,47 @@ public class TestSimpleAnalyticDB {
         }
     }
 
+    @Test
+    public void testMergeSortAnalyticDB() {
+        try {
+            MergeSortAnalyticDB mergeSortAnalyticDB = new MergeSortAnalyticDB();
+            mergeSortAnalyticDB.load(test_data_dir, "./tmp");
+            Map<String, String> resultMap = getResult(test_result_path);
+            commonTest(resultMap, mergeSortAnalyticDB);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private void commonTest(Map<String, String> resultMap, AnalyticDB db) throws Exception {
         for (String key : resultMap.keySet()) {
             String[] columns = key.split(SPLITE);
             String result = db.quantile(columns[0], columns[1], Double.parseDouble(columns[2]));
-//            assertEquals(estimate[estimate.length - 1], resultMap.get(key));
+            assertEquals(resultMap.get(key), result);
             System.out.println("estimate: " + result + " and " + "real: " + resultMap.get(key));
+        }
+    }
+
+    @Test
+    public void testQuery() throws Exception {
+        Map<String, String> resultMap = getResult(test_result_path);
+        RandomAccessFile randomAccessFileOne = new RandomAccessFile("/Users/hgh/Documents/GitHub/tianchi_analyticDB_2021/tmp/lineitem_l_orderkey_sorted", "r");
+        RandomAccessFile randomAccessFileTwo = new RandomAccessFile("/Users/hgh/Documents/GitHub/tianchi_analyticDB_2021/tmp/lineitem_l_partkey_sorted", "r");
+        for (String key : resultMap.keySet()) {
+            String[] columns = key.split(SPLITE);
+            String tableColumn = columns[0] + "_" + columns[1];
+            if (tableColumn.toLowerCase().startsWith("lineitem_l_orderkey")) {
+                randomAccessFileOne.seek((int) (10000 * Double.parseDouble(columns[2]) - 1) * 8);
+                byte[] bbuf = new byte[8];
+                randomAccessFileOne.read(bbuf);
+                assertEquals(Long.parseLong(resultMap.get(key)), bytesToLong(bbuf));
+            }
+            if (tableColumn.toLowerCase().startsWith("lineitem_l_partkey")) {
+                randomAccessFileTwo.seek((int) (10000 * Double.parseDouble(columns[2]) - 1) * 8);
+                byte[] bbuf = new byte[8];
+                randomAccessFileTwo.read(bbuf);
+                assertEquals(Long.parseLong(resultMap.get(key)), bytesToLong(bbuf));
+            }
         }
     }
 
@@ -65,8 +149,6 @@ public class TestSimpleAnalyticDB {
             }
             resultMap.put(stringBuilder.toString(), columns[3]);
         }
-
-        System.out.println();
         return resultMap;
     }
 }
