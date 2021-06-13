3bcf634c2c2feba56817484b409147d26cfd2d55
HuGanghui <huganghui18@gmail.com>
2021-06-13 17:47:50 +0800

    [Passed | PartitionAnalyticDB && 调参] Constant.Buffer_CAP 4096 * 16 ->  4096 * 512

--- src/main/java/com/aliyun/adb/contest/common/Constant.java
+++ src/main/java/com/aliyun/adb/contest/common/Constant.java
@@ -1,6 +1,6 @@
 package com.aliyun.adb.contest.common;
 
 public class Constant {
-    public static final int Buffer_CAP = 4096 * 256;
+    public static final int Buffer_CAP = 4096 * 512;
     public static final int Long_Size = 8;
 }
