c7f75efc636e62bf4771ac2a2bf4a1c53a7f1757
HuGanghui <huganghui18@gmail.com>
2021-06-13 17:56:33 +0800

    [Passed | PartitionAnalyticDB] 完成一次测试

--- src/main/java/com/aliyun/adb/contest/PartitionAnalyticDB.java
+++ src/main/java/com/aliyun/adb/contest/PartitionAnalyticDB.java
@@ -156,15 +156,15 @@ public class PartitionAnalyticDB implements AnalyticDB {
         printTimeAndMemory("saveToDisk", "write into partitionDataLog",
                 startWriteTime, System.currentTimeMillis());
 
-//        for (int i = 0; i < columnLength; i++) {
-//            String key = tableColumns[i];
-//            dataLogSizePrefixSumMap.get(key)[0] =
-//                    dataLogMap.get(tableColumns[i])[0].destroy();
-//            for (int j = 1; j < partitionNum; j++) {
-//                dataLogSizePrefixSumMap.get(key)[j] = dataLogSizePrefixSumMap.get(key)[j-1] +
-//                        dataLogMap.get(tableColumns[i])[j].destroy();
-//            }
-//        }
+        for (int i = 0; i < columnLength; i++) {
+            String key = tableColumns[i];
+            dataLogSizePrefixSumMap.get(key)[0] =
+                    dataLogMap.get(tableColumns[i])[0].destroy();
+            for (int j = 1; j < partitionNum; j++) {
+                dataLogSizePrefixSumMap.get(key)[j] = dataLogSizePrefixSumMap.get(key)[j-1] +
+                        dataLogMap.get(tableColumns[i])[j].destroy();
+            }
+        }
         reader.close();
         printTimeAndMemory("saveToDisk", "saveToDisk ended", startTime, System.currentTimeMillis());
     }
